6933ed9b7df2d1e50c02ae31283f2a0a42b433c8
Argüeso Epelde <i-argueso@euskadi.eus>
2020-06-23 17:11:56 +0200

    version1.2

--- dist/tests.17888de4.js
+++ dist/tests.17888de4.js
@@ -258,7 +258,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54258" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51132" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
